26c2ab6f2426fc858d42c39d4e8d229bde12deb7
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-03-04 18:55:52 +0100

    switch to env

--- LinuxApp.Api/Controllers/WeatherForecastController.cs
+++ LinuxApp.Api/Controllers/WeatherForecastController.cs
@@ -36,7 +36,7 @@ namespace LinuxApp.Api.Controllers
             .ToArray();
         }
 
-        [HttpGet("loafers")]
+        [HttpGet("loaferscreme")]
         public IEnumerable<WeatherForecast> Getters()
         {
             var rng = new Random();
